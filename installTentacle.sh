1ac2869d0bfef68c06ae189d37fbea4f139157d4
Ian hunt <ihuntenator@gmail.com>
2020-12-04 18:58:14 +1300

    added count

--- installTentacle.sh
+++ installTentacle.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
 serverUrl="https://ihuntenator.octopus.app"
 serverCommsPort="10933"
 apiKey="API-SFC3GRXDKOLN8P2B31ASFFASOO8"
+thumbprint="CAAD5651C5B773478D18B0B98AEE8F16A23B2794"
 name=$HOSTNAME
 space="Default"
+environment="development"
+role="test"
 configFilePath="/etc/octopus/default/tentacle-default.config"
 applicationPath="/home/Octopus/Applications/"
 
 curl https://rpm.octopus.com/tentacle.repo -o /etc/yum.repos.d/tentacle.repo
 yum install tentacle -y
 
-sudo /opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath" --instance "$name"
+sudo /opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
 sudo /opt/octopus/tentacle/Tentacle new-certificate --if-blank
 sudo /opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
+sudo /opt/octopus/tentacle/Tentacle configure --trust $thumbprint
 echo "Registering the worker $name with server $serverUrl"
-sudo /opt/octopus/tentacle/Tentacle register-worker --server "$serverUrl" --apiKey "$apiKey" --name "$name"  --comms-style "TentacleActive" --server-comms-port $serverCommsPort --space "$space" --workerpool=Default
+sudo /opt/octopus/tentacle/Tentacle register-with --server "$serverUrl" --apiKey "$apiKey" --name "$name"  --env "$environment"  --comms-style "TentacleActive" --server-comms-port $serverCommsPort --space "$space" --role "$role"
 sudo /opt/octopus/tentacle/Tentacle service --install --start
--- instance.tf
+++ instance.tf
@@ -1,19 +1,15 @@
-# create a Linux instance in AWS
-# execute bash script to set up Ansible client user
-
 resource "aws_instance" "linux_instance" {
   ami             = lookup(var.amis, var.region) 
+  count           = 1
   subnet_id       = var.subnet 
   security_groups = var.securityGroups 
   key_name        = var.keyName 
   instance_type   = var.instanceType 
   
-  # Name the instance
   tags = {
     Name = var.instanceName
   }
-  # Name the volumes; will name all volumes included in the 
-  # ami and the ebs block device from above with this instance.
+
   volume_tags = {
     Name = var.instanceName
   }
@@ -36,4 +32,4 @@ resource "aws_instance" "linux_instance" {
     private_key = file(var.keyPath)
     host        = self.public_ip
   }
-} # end resource
+} 
