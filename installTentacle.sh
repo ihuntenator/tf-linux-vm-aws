d5467db693bbfbff2d888e0c2e45f9f0dac71b40
Ian hunt <ihuntenator@gmail.com>
2020-12-05 16:19:30 +1300

    nolisten false

--- installTentacle.sh
+++ installTentacle.sh
@@ -16,7 +16,7 @@ yum install tentacle -y
 
 sudo /opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
 sudo /opt/octopus/tentacle/Tentacle new-certificate --if-blank
-sudo /opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
+sudo /opt/octopus/tentacle/Tentacle configure --noListen False --reset-trust --app "$applicationPath"
 sudo /opt/octopus/tentacle/Tentacle configure --trust $thumbprint
 #echo "Registering the worker $name with server $serverUrl"
 #sudo /opt/octopus/tentacle/Tentacle register-with --server "$serverUrl" --apiKey "$apiKey" --name "$name"  --env "$environment"  --comms-style "TentacleActive" --server-comms-port $serverCommsPort --space "$space" --role "$role"
